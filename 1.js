10d04dd0b11e3b9b43cac5527f1a9eed9e161640
Punicode <nguyentuangl96@gmail.com>
2020-04-04 14:31:03 +0700

    load4

--- 1.js
+++ 1.js
@@ -20,5 +20,90 @@ function kt(){
     if(document.getElementById("check8").checked)k++;
     if(document.getElementById("check9").checked)k++;
     if(document.getElementById("check10").checked)k++;
+    if(k<10&&k>=5)
     alert("CHÚC MỪNG BẠN ĐẠT ĐƯỢC "+k+"/10. HÃY CỐ GẮNG ĐỂ HOÀN THIỆN HƠN NHÉ");
+    else if(k==10) alert("Perfect! "+k+"/10");
+    else alert(k+"/10 Hơi buồn! Hãy cố gắng lên nào!");
+}
+function doiborder(){
+    if (document.getElementById("check1").checked) {
+        document.getElementById("nut1").style.border =" 5px solid green";
+        document.getElementById("nut1").style.padding="15px";
+    }
+    else {
+        document.getElementById("nut1").style.border = "none";
+        document.getElementById("nut1").style.padding = "20px";
+    }
+    if (document.getElementById("check2").checked) {
+        document.getElementById("nut2").style.border =" 5px solid green";
+        document.getElementById("nut2").style.padding="15px";
+    }
+    else {
+        document.getElementById("nut2").style.border = "none";
+        document.getElementById("nut2").style.padding = "20px";
+    }
+    if (document.getElementById("check3").checked) {
+        document.getElementById("nut3").style.border = " 5px solid green";
+        document.getElementById("nut3").style.padding = "15px";
+    }
+    else {
+        document.getElementById("nut3").style.border = "none";
+        document.getElementById("nut3").style.padding = "20px";
+    }
+    if (document.getElementById("check4").checked) {
+        document.getElementById("nut4").style.border = " 5px solid green";
+        document.getElementById("nut4").style.padding = "15px";
+    }
+    else {
+        document.getElementById("nut4").style.border = "none";
+        document.getElementById("nut4").style.padding = "20px";
+    }
+    if (document.getElementById("check5").checked) {
+        document.getElementById("nut5").style.border = " 5px solid green";
+        document.getElementById("nut5").style.padding = "15px";
+    }
+    else {
+        document.getElementById("nut5").style.border = "none";
+        document.getElementById("nut5").style.padding = "20px";
+    }
+    if (document.getElementById("check6").checked) {
+        document.getElementById("nut6").style.border = " 5px solid green";
+        document.getElementById("nut6").style.padding = "15px";
+    }
+    else {
+        document.getElementById("nut6").style.border = "none";
+        document.getElementById("nut6").style.padding = "20px";
+    }
+    if (document.getElementById("check7").checked) {
+        document.getElementById("nut7").style.border = " 5px solid green";
+        document.getElementById("nut7").style.padding = "15px";
+    }
+    else {
+        document.getElementById("nut7").style.border = "none";
+        document.getElementById("nut7").style.padding = "20px";
+    }
+    if (document.getElementById("check8").checked) {
+        document.getElementById("nut8").style.border = " 5px solid green";
+        document.getElementById("nut8").style.padding = "15px";
+    }
+    else {
+        document.getElementById("nut8").style.border = "none";
+        document.getElementById("nut8").style.padding = "20px";
+    }
+    if (document.getElementById("check9").checked) {
+        document.getElementById("nut9").style.border = " 5px solid green";
+        document.getElementById("nut9").style.padding = "15px";
+    }
+    else {
+        document.getElementById("nut9").style.border = "none";
+        document.getElementById("nut9").style.padding = "20px";
+    }
+    if (document.getElementById("check10").checked) {
+        document.getElementById("nut10").style.border = " 5px solid green";
+        document.getElementById("nut10").style.padding = "15px";
+    }
+    else {
+        document.getElementById("nut10").style.border = "none";
+        document.getElementById("nut10").style.padding = "20px";
+    }
 }
